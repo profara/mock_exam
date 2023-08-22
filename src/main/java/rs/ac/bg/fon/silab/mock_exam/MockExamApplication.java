be6012827f905754c330408a4606c7d9447e7d79
profara <alekbg99@gmail.com>
2023-08-22 23:21:59 +0200

    Initial commit

---
+++ src/main/java/rs/ac/bg/fon/silab/mock_exam/MockExamApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.fon.silab.mock_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rs/ac/bg/fon/silab/mock_exam/MockExamApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.fon.silab.mock_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
