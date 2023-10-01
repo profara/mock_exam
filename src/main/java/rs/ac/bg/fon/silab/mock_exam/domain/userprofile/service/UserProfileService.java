a3db94d797e57fe12a4eba7ea9a80ba24a2dd63b
profara <alekbg99@gmail.com>
2023-10-01 11:15:09 +0200

    Merge

--- src/main/java/rs/ac/bg/fon/silab/mock_exam/domain/userprofile/service/UserProfileService.java
+++ src/main/java/rs/ac/bg/fon/silab/mock_exam/domain/userprofile/service/UserProfileService.java
@@ -2,7 +2,6 @@ package rs.ac.bg.fon.silab.mock_exam.domain.userprofile.service;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import rs.ac.bg.fon.silab.mock_exam.domain.userprofile.dto.UserProfileRequestDTO;
 import rs.ac.bg.fon.silab.mock_exam.domain.userprofile.dto.UserProfileResponseDTO;
 import rs.ac.bg.fon.silab.mock_exam.domain.userprofile.dto.UserProfileUpdateRoleRequestDTO;
 import rs.ac.bg.fon.silab.mock_exam.domain.userprofile.dto.UserProfileRequestUpdateDTO;
