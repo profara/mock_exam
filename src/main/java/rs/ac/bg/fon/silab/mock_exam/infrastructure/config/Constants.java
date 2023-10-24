6c7ca722de2d8d08b76398af143d40d871e8a810
profara <alekbg99@gmail.com>
2023-10-24 19:02:16 +0200

    Ispravljen link

--- src/main/java/rs/ac/bg/fon/silab/mock_exam/infrastructure/config/Constants.java
+++ src/main/java/rs/ac/bg/fon/silab/mock_exam/infrastructure/config/Constants.java
@@ -36,5 +36,5 @@ public class Constants {
     public static final Long MATH_EXAM_ID = 1L;
     public static final Long OPSTA_INF_EXAM_ID = 3L;
 
-    public static final String CONFIRMATION_LINK = "http://localhost:8080/api/userProfiles/confirm?token=";
+    public static final String CONFIRMATION_LINK = "http://localhost:5173/uspesnoPotvrdjen";
 }
