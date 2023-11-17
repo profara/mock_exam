bd2b09dba794c1b1c79b0513a1dbe211766c98e7
profara <alekbg99@gmail.com>
2023-11-17 14:40:18 +0100

    Napisan JavaDoc za UserProfileController

--- src/main/java/rs/ac/bg/fon/silab/mock_exam/web/UserProfileController.java
+++ src/main/java/rs/ac/bg/fon/silab/mock_exam/web/UserProfileController.java
@@ -12,24 +12,33 @@ import rs.ac.bg.fon.silab.mock_exam.domain.userprofile.dto.UserProfileRequestUpd
 import rs.ac.bg.fon.silab.mock_exam.domain.userprofile.dto.UserProfileResponseDTO;
 import rs.ac.bg.fon.silab.mock_exam.domain.userprofile.dto.UserProfileUpdateRoleRequestDTO;
 import rs.ac.bg.fon.silab.mock_exam.domain.userprofile.service.UserProfileService;
-import rs.ac.bg.fon.silab.mock_exam.infrastructure.email.EmailSender;
-import rs.ac.bg.fon.silab.mock_exam.infrastructure.jwt.JWTUtil;
 
-import static rs.ac.bg.fon.silab.mock_exam.infrastructure.config.Constants.CONFIRMATION_LINK;
 
+/**
+ * REST controller for managing user profile operations.
+ * Provides endpoints for creating, retrieving, updating, deleting, and confirming user profiles.
+ */
 @RestController
 @RequestMapping("/api/userProfiles")
 public class UserProfileController {
 
     private final UserProfileService userProfileService;
-    private final JWTUtil jwtUtil;
 
-
-    public UserProfileController(UserProfileService userProfileService, JWTUtil jwtUtil) {
+    /**
+     * Constructs a UserProfileController with necessary UserProfileService and JWTUtil dependencies.
+     *
+     * @param userProfileService The service handling user profile operations.
+     */
+    public UserProfileController(UserProfileService userProfileService) {
         this.userProfileService = userProfileService;
-        this.jwtUtil = jwtUtil;
     }
 
+    /**
+     * Endpoint to save a new user profile.
+     *
+     * @param userProfileDTO DTO containing the user profile details.
+     * @return ResponseEntity with the JWT token in the header.
+     */
     @PostMapping
     public ResponseEntity<?> save(@Valid @RequestBody UserProfileRequestUpdateDTO userProfileDTO){
         RegistrationResponseDTO registrationResponseDTO = userProfileService.save(userProfileDTO);
@@ -41,37 +50,80 @@ public class UserProfileController {
     }
 
 
+    /**
+     * Endpoint to retrieve a user profile by its ID.
+     *
+     * @param id The ID of the user profile to retrieve.
+     * @return ResponseEntity with OK status and the found UserProfileResponseDTO.
+     */
     @GetMapping("/{id}")
     public ResponseEntity<UserProfileResponseDTO> getById(@PathVariable Long id){
         return ResponseEntity.ok(userProfileService.getById(id));
     }
 
+    /**
+     * Endpoint to retrieve a paginated list of user profiles.
+     *
+     * @param pageable Pagination information.
+     * @return ResponseEntity with OK status and a page of UserProfileResponseDTOs.
+     */
     @GetMapping
     public ResponseEntity<Page<UserProfileResponseDTO>> get(Pageable pageable){
         return ResponseEntity.ok(userProfileService.get(pageable));
     }
 
+    /**
+     * Endpoint to retrieve a user profile by email.
+     *
+     * @param email The email of the user profile to retrieve.
+     * @return ResponseEntity with OK status and the found UserProfileResponseDTO.
+     */
     @GetMapping(params = "email")
     public ResponseEntity<UserProfileResponseDTO> getByEmail(@RequestParam String email){
         return ResponseEntity.ok(userProfileService.getByEmail(email));
     }
 
+    /**
+     * Endpoint to confirm a user profile based on the provided token.
+     *
+     * @param token The token used for confirmation.
+     * @return ResponseEntity with OK status and the confirmed UserProfileResponseDTO.
+     */
     @GetMapping("/confirm")
     public ResponseEntity<UserProfileResponseDTO> confirm(@RequestParam("token") String token){
         return ResponseEntity.ok(userProfileService.enableProfile(token));
     }
 
+    /**
+     * Endpoint to delete a user profile by its ID.
+     *
+     * @param id The ID of the user profile to delete.
+     */
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id){
         userProfileService.delete(id);
     }
 
+    /**
+     * Endpoint to update the role of a user profile.
+     *
+     * @param id The ID of the user profile to update.
+     * @param userProfileDTO DTO containing the new role information.
+     * @return ResponseEntity with OK status and the updated UserProfileResponseDTO.
+     */
     @PatchMapping("/changeRole/{id}")
     public ResponseEntity<UserProfileResponseDTO> updateUserRole(@PathVariable Long id, @Valid @RequestBody UserProfileUpdateRoleRequestDTO userProfileDTO){
         return ResponseEntity.ok(userProfileService.updateUserRole(id,userProfileDTO));
     }
 
+    /**
+     * Endpoint to update a user profile.
+     *
+     * @param id The ID of the user profile to update.
+     * @param userProfileDTO DTO containing the updated user profile details.
+     * @return ResponseEntity with OK status and the updated UserProfileResponseDTO.
+     */
     @PatchMapping("/{id}")
     public ResponseEntity<UserProfileResponseDTO> update(@PathVariable Long id, @Valid @RequestBody UserProfileRequestUpdateDTO userProfileDTO){
         return ResponseEntity.ok(userProfileService.update(id,userProfileDTO));
